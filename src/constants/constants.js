380fe68c60300f98d6d5ecda434c604756875068
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-02-10 10:22:20 +0500

    add project in webiste

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "Notification in React",
+    description:
+      "Using React, Node.js, Express & and firebase I have create an Application in React Native. were I sent Notification to user Mobile.",
+    image: "/images/1.png",
+    tags: ["Firebase", "Express", "React", "Node"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "Real Time Chat",
+    description:
+      "I have created facebook Messenger clone. I used socket io and implement real Time chat without Load or Refresh the page. ",
+    image: "/images/2.png",
+    tags: ["React", "Mongodb", "socket io", "node", "Express"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Memories Website",
+    description:
+      "Using MERN techinque I have created a website where one can Login with google or email. after login user can post, like, delete, and see recommended posts.",
+    image: "/images/3.jpg",
+    tags: ["React", "Express", "Mongodb", "MaterialUi", "Node"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Weather App in React Native",
+    description:
+      "using React Native I have implemented an application where we can enter any city and see the current temprature of that city or state.",
+    image: "/images/4.jpg",
+    tags: ["React Native", "React native Paper"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
