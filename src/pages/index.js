d7ab072d328da4b3e0ff9d41d7dc78d84d9243c6
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-02-11 08:38:31 +0500

    fixed errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
+import Hero from '../components/hero/Hero';
 import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
+import Technologies from '../components/technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
